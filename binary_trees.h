1ea88de7fa8e1d3efc9e9c835826796900b5ea89
Marisol2201 <1529@holbertonschool.com>
2020-07-20 00:44:38 +0000

    tasks 0 and 1 added

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <string.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -27,5 +29,6 @@ typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */
