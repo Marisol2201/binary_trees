13ce4a1dcf3563c769af41e03f0247bd9d148898
peluza <edisonisaza@gmail.com>
2020-07-22 20:34:48 -0500

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
  * *binary_trees_ancestor -   finds the lowest common ancestor of two nodes
  * @first:  is a pointer to the first node
@@ -8,32 +9,26 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second)
 {
-	while (binary_tree_height(second) != binary_tree_height(first))
+	const binary_tree_t *curr1 = NULL, *curr2 = NULL;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	if (first == second)
+		return ((binary_tree_t *)second);
+
+	curr1 = first;
+
+	while (curr1)
 	{
-		if (binary_tree_height(first) == binary_tree_height(second))
-			return (second->parent);
-		while (binary_tree_height(first) > binary_tree_height(second))
+		curr2 = second;
+		while (curr2)
 		{
-			second = first;
-			return (second->parent);
+			if (curr1 == curr2)
+				return ((binary_tree_t *)curr1);
+			curr2 = curr2->parent;
 		}
-		return (second->parent);
+		curr1 = curr1->parent;
 	}
-	return (second->parent);
-}
-/**
- * binary_tree_height -  measures the height of a binary tree
- * @tree: is a pointer to the root node of the tree to measure the height
- * Return: If tree is NULL, your function must return 0
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t height_left, height_right;
-	if (tree == NULL || (tree->right == NULL && tree->left == NULL))
-		return (0);
-	height_left = 1 + binary_tree_height(tree->left);
-	height_right = 1 + binary_tree_height(tree->right);
-	if (height_left > height_right)
-		return (height_left);
-	return (height_right);
+	return (NULL);
 }
