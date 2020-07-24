2e6dc44ca86aef522c8141d585a997fd5e9607f0
Marisol2201 <1529@holbertonschool.com>
2020-07-24 20:16:08 +0000

    task 23 with a condition if tree->left exists

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -8,14 +8,21 @@
 
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
+	/*binary_tree_t *aux;*/
+	/*binary_tree_t *new_right;*/
+	/*binary_tree_t *new_left;*/
 	binary_tree_t *new_tree;
+	/*binary_tree_t *new_parent;*/
 
 	if (!tree || !tree->left)
 		return (NULL);
 
 	new_tree = tree->left;
 	tree->left = new_tree->right;
+	if (tree->left)
+		tree->left->parent = tree;
 	new_tree->right = tree;
+	new_tree->parent = tree->parent;
 	tree->parent = new_tree;
 
 	return (new_tree);
